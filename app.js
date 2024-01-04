d5aaa959ba8060a05e2a0958cc69e4b63473060a
HoseokYoo <hsyoo@pmirnc.com>
2024-01-04 13:06:04 +0900

    feat(init): init
    init

---
+++ app.js
@@ -0,0 +1,25 @@
+const { select, checkbox } = require('@inquirer/prompts');
+const { parse } = require('yaml');
+const fs = require('fs');
+const path = require('path');
+(async () => {
+    console.log(process.pkg);
+    function convertOptions(options) {
+        const { label, value, description } = options;
+        return { name: label, value, description: description || label };
+    }
+
+    function generateQuestions(question) {
+        const { index, content: message, options, type } = question;
+        if (type === 'checkbox') return checkbox({ message, choices: options.map(convertOptions) });
+        return select({ message, choices: options.map(convertOptions) });
+    }
+
+    const assetsPath = path.join(__dirname,'assets/questions.yaml');
+    const { questions } = parse(fs.readFileSync(assetsPath, 'utf8'));
+
+    for (const question of questions) {
+        const { index, content: message, options, type } = question;
+        const answer = await generateQuestions(question);
+    }
+})();
